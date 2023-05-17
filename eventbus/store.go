83faa5ec6d516399c63443a9e84be0bc25f836a1
waite1x <waite1x@outlook.com>
2023-05-17 18:16:51 +0800

    fix: Fix event-bus listening build error

--- eventbus/store.go
+++ eventbus/store.go
@@ -26,7 +26,7 @@ type EventStore interface {
 	// Save saves an event to the event store
 	Save(ctx context.Context, data *EventMsg) error
 
-	GetList(ctx context.Context, p *x.PageParam) ([]EventMsg, error)
+	GetList(ctx context.Context, p *x.PageParam) (x.PagedResult[EventMsg], error)
 	Delete(ctx context.Context, id []primitive.ObjectID) error
 }
 
@@ -41,33 +41,26 @@ func ListenErrorEvents(clock int) {
 		//以下为定时执行的操作
 		if store, ok := app.GetOptional[EventStore](); ok {
 			page := x.NewPageParam(1, 100)
-			page.SetLimitPlus(1)
 			for {
-				hasNext := false
 				events, err := (*store).GetList(context.Background(), page)
 				if err != nil {
 					log.Warnf("get error events failed, error: %s", err.Error())
 					break
 				}
-				if len(events) < int(page.PageSize) {
-					hasNext = true
-				} else {
-					events = events[:len(events)-1]
-				}
-				for _, event := range events {
+				for _, event := range events.Data {
 					if event.ErrorMsg == testError {
 						continue
 					}
 					PublishJSON(context.Background(), event.Topic, event.Data)
 				}
-				ids := linq.Map(events, func(event *EventMsg) primitive.ObjectID {
+				ids := linq.Map(events.Data, func(event *EventMsg) primitive.ObjectID {
 					return event.ID
 				})
 				err = (*store).Delete(context.Background(), ids)
 				if err != nil {
 					log.Warnf("delete error events failed, error: %s", err.Error())
 				}
-				if hasNext {
+				if !events.HasMore {
 					break
 				} else {
 					page.Page++
