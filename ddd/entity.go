0ab53a8dfac6f5ab5caa0b164ff002d157161116
waite <waite1x@outlook.com>
2023-05-29 18:06:14 +0800

    feat: Update WithExtraEntity struct

--- ddd/entity.go
+++ ddd/entity.go
@@ -37,36 +37,36 @@ func (e *EntityBase) SetId(id string) {
 }
 
 type WithExtraEntity struct {
-	Data map[string]string `bson:"data,omitempty"`
+	Extra map[string]string `bson:"extra,omitempty"`
 }
 
-func (m *WithExtraEntity) SetProperty(key string, value interface{}) {
-	if m.Data == nil {
-		m.Data = make(map[string]string)
+func (m *WithExtraEntity) SetValue(key string, v interface{}) {
+	if m.Extra == nil {
+		m.Extra = make(map[string]string)
 	}
-	content, err := json.Marshal(value)
+	content, err := json.Marshal(v)
 	if err == nil {
-		m.Data[key] = string(content)
+		m.Extra[key] = string(content)
 	}
 }
 
-func (m *WithExtraEntity) GetProperty(key string, value interface{}) (bool, error) {
-	if m.Data == nil {
+func (m *WithExtraEntity) GetValue(key string, v interface{}) (bool, error) {
+	if m.Extra == nil {
 		return false, nil
 	}
-	content, ok := m.Data[key]
+	content, ok := m.Extra[key]
 	if ok {
-		err := json.Unmarshal([]byte(content), value)
+		err := json.Unmarshal([]byte(content), v)
 		return true, err
 	}
 	return false, nil
 }
 
 func (m *WithExtraEntity) GetString(key string) string {
-	if m.Data == nil {
+	if m.Extra == nil {
 		return ""
 	}
-	content, ok := m.Data[key]
+	content, ok := m.Extra[key]
 	if ok {
 		return content
 	}
@@ -74,17 +74,17 @@ func (m *WithExtraEntity) GetString(key string) string {
 }
 
 func (m *WithExtraEntity) SetString(key string, value string) {
-	if m.Data == nil {
-		m.Data = make(map[string]string)
+	if m.Extra == nil {
+		m.Extra = make(map[string]string)
 	}
-	m.Data[key] = value
+	m.Extra[key] = value
 }
 
 func (m *WithExtraEntity) GetInt(key string) int {
-	if m.Data == nil {
+	if m.Extra == nil {
 		return 0
 	}
-	content, ok := m.Data[key]
+	content, ok := m.Extra[key]
 	if ok {
 		v, err := strconv.Atoi(content)
 		errx.CheckError(err)
@@ -94,17 +94,17 @@ func (m *WithExtraEntity) GetInt(key string) int {
 }
 
 func (m *WithExtraEntity) SetInt(key string, value int) {
-	if m.Data == nil {
-		m.Data = make(map[string]string)
+	if m.Extra == nil {
+		m.Extra = make(map[string]string)
 	}
-	m.Data[key] = strconv.Itoa(value)
+	m.Extra[key] = strconv.Itoa(value)
 }
 
 func (m *WithExtraEntity) GetFloat32(key string) float32 {
-	if m.Data == nil {
+	if m.Extra == nil {
 		return 0
 	}
-	content, ok := m.Data[key]
+	content, ok := m.Extra[key]
 	if ok {
 		v, err := strconv.ParseFloat(content, 32)
 		errx.CheckError(err)
@@ -114,10 +114,10 @@ func (m *WithExtraEntity) GetFloat32(key string) float32 {
 }
 
 func (m *WithExtraEntity) SetFloat32(key string, value float32) {
-	if m.Data == nil {
-		m.Data = make(map[string]string)
+	if m.Extra == nil {
+		m.Extra = make(map[string]string)
 	}
-	m.Data[key] = strconv.FormatFloat(float64(value), 'E', -1, 32)
+	m.Extra[key] = strconv.FormatFloat(float64(value), 'E', -1, 32)
 }
 
 type WithExtraArrayEntity struct {
