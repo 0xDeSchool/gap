7be5e3793a2862d843241e0c1ccc069763a63e3b
waite <waite1x@outlook.com>
2023-09-04 14:40:58 +0800

    fix: Fix id generator

--- x/id.go
+++ x/id.go
@@ -18,48 +18,48 @@ type IdGenerator[T comparable] interface {
 	Create() T
 }
 
-type StringIdGenerator struct {
+type StringIdGenerator[T string] struct {
 }
 
-func NewStringIdGenerator() *IdGenerator[string] {
+func NewStringIdGenerator[T string]() *IdGenerator[T] {
 	workerId := app.Get[IdGeneratorOptions]().WorkerId
 	var options = idgen.NewIdGeneratorOptions(workerId)
 	// 保存参数（务必调用，否则参数设置不生效）：
 	idgen.SetIdGenerator(options)
-	var g IdGenerator[string] = &StringIdGenerator{}
+	var g IdGenerator[T] = &StringIdGenerator[T]{}
 	return &g
 }
 
-func (u *StringIdGenerator) Create() string {
+func (u *StringIdGenerator[T]) Create() T {
 	id := idgen.NextId()
 	idStr, err := basex.EncodeInt(big.NewInt(id))
 	if err != nil {
 		panic(err)
 	}
-	return idStr
+	return T(idStr)
 }
 
-type Int64IdGenerator struct {
+type Int64IdGenerator[T int64] struct {
 }
 
-func NewNumberIdGenerator() *IdGenerator[int64] {
+func NewNumberIdGenerator[T int64]() *IdGenerator[T] {
 	workerId := app.Get[IdGeneratorOptions]().WorkerId
 	var options = idgen.NewIdGeneratorOptions(workerId)
 	// 保存参数（务必调用，否则参数设置不生效）：
 	idgen.SetIdGenerator(options)
-	var g IdGenerator[int64] = &Int64IdGenerator{}
+	var g IdGenerator[T] = &Int64IdGenerator[T]{}
 	return &g
 }
 
-func (u *Int64IdGenerator) Create() int64 {
-	return idgen.NextId()
+func (u *Int64IdGenerator[T]) Create() T {
+	return T(idgen.NextId())
 }
 
 func DefaultIdGenerators(ab *app.AppBuilder) {
 	ab.ConfigureServices(func() error {
 		app.TryAddValue(&IdGeneratorOptions{WorkerId: 1})
-		app.TryAddSingleton(NewStringIdGenerator)
-		app.TryAddSingleton(NewNumberIdGenerator)
+		app.TryAddSingleton(NewStringIdGenerator[string])
+		app.TryAddSingleton(NewNumberIdGenerator[int64])
 		return nil
 	})
 }
