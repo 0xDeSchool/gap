fcc1b08b3620b30c92d12cdcb2d7cdb0bf1e2c98
waite1x <waite1x@outlook.com>
2023-10-12 15:39:06 +0800

    fix: 更新 aws options 方式

--- aws/aws.go
+++ aws/aws.go
@@ -7,19 +7,55 @@ import (
 	"github.com/aws/aws-sdk-go-v2/credentials"
 )
 
-type Options struct {
-	BaseUrl         string
-	AccessKeyId     string
-	SecretAccessKey string
-	SessionToken    string
-	Region          string
+type Options map[string]any
+
+func (opts Options) GetString(k string) string {
+	if v, ok := opts[k].(string); ok {
+		return v
+	}
+	return ""
+}
+
+func (opts Options) BaseUrl() string {
+	return opts.GetString("BaseUrl")
+}
+func (opts Options) SetBaseUrl(v string) {
+	opts["BaseUrl"] = v
+}
+
+func (opts Options) AccessKeyId() string {
+	return opts.GetString("AccessKeyId")
+}
+func (opts Options) SetAccessKeyId(v string) {
+	opts["AccessKeyId"] = v
+}
+
+func (opts Options) SecretAccessKey() string {
+	return opts.GetString("SecretAccessKey")
+}
+func (opts Options) SetSecretAccessKey(v string) {
+	opts["SecretAccessKey"] = v
+}
+
+func (opts Options) SessionToken() string {
+	return opts.GetString("SessionToken")
+}
+func (opts Options) SetSessionToken(v string) {
+	opts["SessionToken"] = v
+}
+
+func (opts Options) Region() string {
+	return opts.GetString("Region")
+}
+func (opts Options) SetRegion(v string) {
+	opts["Region"] = v
 }
 
 func loadAwsConfig(opts *Options) (*aws.Config, error) {
-	p := credentials.NewStaticCredentialsProvider(opts.AccessKeyId, opts.SecretAccessKey, opts.SessionToken)
+	p := credentials.NewStaticCredentialsProvider(opts.AccessKeyId(), opts.SecretAccessKey(), opts.SessionToken())
 	cfg, err := config.LoadDefaultConfig(
 		context.TODO(),
-		config.WithRegion(opts.Region),
+		config.WithRegion(opts.Region()),
 		config.WithCredentialsProvider(p),
 	)
 	return &cfg, err
--- aws/s3.go
+++ aws/s3.go
@@ -6,6 +6,16 @@ import (
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 )
 
+// Bucket s3 client options
+func (opts Options) Bucket() string {
+	return opts.GetString("Bucket")
+}
+
+// SetBucket s3 client options
+func (opts Options) SetBucket(v string) {
+	opts["Bucket"] = v
+}
+
 func addS3Client() {
 	app.TryAddSingleton(func() *s3.Client {
 		conf := app.Get[aws.Config]()
