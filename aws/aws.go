295675575b234cb9c1b525cd4839832fcd253f94
waite1x <waite1x@outlook.com>
2023-10-12 20:59:46 +0800

    fix: fix

--- aws/aws.go
+++ aws/aws.go
@@ -5,20 +5,21 @@ import (
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/credentials"
+	"strings"
 )
 
 type Options map[string]any
 
 func (opts Options) Set(k string, v any) {
-	opts[k] = v
+	opts[strings.ToLower(k)] = v
 }
 
 func (opts Options) Get(k string) any {
-	return opts[k]
+	return opts[strings.ToLower(k)]
 }
 
 func (opts Options) GetString(k string) string {
-	if v, ok := opts[k].(string); ok {
+	if v, ok := opts[strings.ToLower(k)].(string); ok {
 		return v
 	}
 	return ""
@@ -28,35 +29,35 @@ func (opts Options) BaseUrl() string {
 	return opts.GetString("BaseUrl")
 }
 func (opts Options) SetBaseUrl(v string) {
-	opts["BaseUrl"] = v
+	opts.Set("BaseUrl", v)
 }
 
 func (opts Options) AccessKeyId() string {
 	return opts.GetString("AccessKeyId")
 }
 func (opts Options) SetAccessKeyId(v string) {
-	opts["AccessKeyId"] = v
+	opts.Set("AccessKeyId", v)
 }
 
 func (opts Options) SecretAccessKey() string {
 	return opts.GetString("SecretAccessKey")
 }
 func (opts Options) SetSecretAccessKey(v string) {
-	opts["SecretAccessKey"] = v
+	opts.Set("SecretAccessKey", v)
 }
 
 func (opts Options) SessionToken() string {
 	return opts.GetString("SessionToken")
 }
 func (opts Options) SetSessionToken(v string) {
-	opts["SessionToken"] = v
+	opts.Set("SessionToken", v)
 }
 
 func (opts Options) Region() string {
 	return opts.GetString("Region")
 }
 func (opts Options) SetRegion(v string) {
-	opts["Region"] = v
+	opts.Set("Region", v)
 }
 
 func loadAwsConfig(opts *Options) (*aws.Config, error) {
